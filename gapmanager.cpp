5f65fe58b63bd00a523efd7f03a2e20921a5f0c2
uyras <150030@mail.ru>
2016-04-17 15:37:27 +1000

    Добавлено привильное логарифмическое распределение гапов, и лограспределение в обе стороны

--- gapmanager.cpp
+++ gapmanager.cpp
@@ -32,7 +32,88 @@ void GapManager::setUniform(unsigned gaps, unsigned intervals, double overlap)
     tos[gaps-1]=intervals-1; //последний гап обязательно последний интервал
 }
 
-void GapManager::setLogarithmic(unsigned gaps, unsigned intervals, double alfa, double beta)
+void GapManager::setLogarithmic(const unsigned gaps, const unsigned intervals, const double overlap, const double reductionSpeed)
+{
+    this->gaps = gaps;
+    this->intervals=intervals;
+    froms.resize(gaps);
+    tos.resize(gaps);
+
+    double kPow=0;
+    const double ooverlap=1.-overlap;
+    for (unsigned i=0; i<gaps; i++)
+        kPow=reductionSpeed*kPow+1.;
+
+    double l=(double)intervals/(ooverlap*kPow+overlap*pow(reductionSpeed,gaps-1));
+
+    for (unsigned i=0; i<gaps; i++){
+        kPow=0;
+        for (unsigned j=0; j<i; j++){
+            kPow=reductionSpeed*kPow+1.;
+        }
+        tos[gaps-1-i]=(double)(intervals-1)-(l*kPow*ooverlap);
+        froms[gaps-1-i]=(double)(intervals-1)-(l*kPow*ooverlap+l*pow(reductionSpeed,i));
+    }
+
+    froms[0]=0;
+    tos[gaps-1]=intervals-1;
+}
+
+void GapManager::setLogarithmicBothSides(const unsigned gaps, const unsigned intervals, const double overlap, const double reductionSpeed)
+{
+    this->gaps = gaps;
+    this->intervals=intervals;
+    froms.resize(gaps);
+    tos.resize(gaps);
+
+    unsigned halfGaps=floor(gaps/2);
+    unsigned halfIntervals=floor(intervals/2);
+
+    double kPow=0;
+    const double ooverlap=1.-overlap;
+
+    for (unsigned i=0; i<halfGaps; i++)
+        kPow=reductionSpeed*kPow+1.;
+
+    double l=(double)halfIntervals/(ooverlap*kPow+overlap*pow(reductionSpeed,halfGaps-1));
+
+    //первая половина гапов - увеличение интервала
+    for (unsigned i=0; i<halfGaps; i++){
+        kPow=0;
+        for (unsigned j=0; j<i; j++){
+            kPow=reductionSpeed*kPow+1.;
+        }
+        tos[halfGaps-1-i]=(double)(halfIntervals-1)-(l*kPow*ooverlap);
+        froms[halfGaps-1-i]=(double)(halfIntervals-1)-(l*kPow*ooverlap+l*pow(reductionSpeed,i));
+    }
+
+    unsigned middlePoint=halfGaps;
+    if (gaps%2)
+        middlePoint=halfGaps+1;
+
+    //вторая половина - уменьшение гапа
+    for (unsigned i=0; i<halfGaps; i++){
+        kPow=0;
+        for (unsigned j=0; j<i; j++){
+            kPow=reductionSpeed*kPow+1.;
+        }
+        froms[middlePoint+i]=halfIntervals+(l*kPow*ooverlap);
+        tos[middlePoint+i]=halfIntervals+(l*kPow*ooverlap+l*pow(reductionSpeed,i));
+    }
+
+    if (gaps%2){ //если четное число гапов, задаем среднее
+        froms[halfGaps]=froms[halfGaps+1]-(tos[halfGaps+1]-froms[halfGaps+1])*overlap;
+        tos[halfGaps]=tos[halfGaps-1]+(tos[halfGaps-1]-froms[halfGaps-1])*overlap;
+    } else {//если нечетное число гапов, расширяем средние
+        tos[halfGaps-1]+=(tos[halfGaps-1]-froms[halfGaps-1])*overlap;
+        froms[halfGaps]-=(tos[halfGaps]-froms[halfGaps])*overlap;
+    }
+
+    froms[0]=0;
+    tos[gaps-1]=intervals-1;
+}
+
+void GapManager::setLogarithmic2(unsigned gaps, unsigned intervals, double alfa, double beta)
 {
     this->gaps = gaps;
     this->intervals=intervals;
@@ -87,4 +168,18 @@ double GapManager::to(unsigned gap)
 double GapManager::inRange(unsigned interval, unsigned gap)
 {
     return interval>=froms[gap] && interval<=tos[gap];
+}
+
+string GapManager::toWolframString()
+{
+    ostringstream s;
+    bool first=true;
+    for (unsigned i=0;i<Gaps();i++){
+        if (!first)
+            s<<",";
+        s<<"{{"<<this->from(i)<<","<<i<<"},";
+        s<<"{"<<this->to(i)<<","<<i<<"}}";
+        first=false;
+    }
+    return s.str();
 }
--- gapmanager.h
+++ gapmanager.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <cmath>
+#include <sstream>
 
 using namespace std;
 class GapManager
@@ -12,14 +13,18 @@ public:
     GapManager(unsigned gaps, unsigned intervals, double overlap);
 
     void setUniform(unsigned gaps, unsigned intervals, double overlap);
+
+    void setLogarithmic(const unsigned gaps, const unsigned intervals, const double overlap=0.6, const double reductionSpeed=0.8);
+    void setLogarithmicBothSides(const unsigned gaps, const unsigned intervals, const double overlap=0.6, const double reductionSpeed=0.8);
+
     /**
-     * @brief setLogarithmic Устанавливает логарифмически увеличивающуюся шкалу
+     * @brief setLogarithmic2 Устанавливает логарифмически увеличивающуюся шкалу
      * @param gaps Число энергетических зон шкалы
      * @param intervals Число интервалов шкалы
      * @param alfa Скоростьсдвига интервала относительно соседнего
      * @param beta Скорость увеличения ширины интервала
      */
-    void setLogarithmic(unsigned gaps, unsigned intervals, double alfa=2., double beta=1.);
+    void setLogarithmic2(unsigned gaps, unsigned intervals, double alfa=2., double beta=1.);
 
     void setLinear(unsigned gaps, unsigned intervals, double overlap=0.8);
 
@@ -29,6 +34,8 @@ public:
 
     inline unsigned Gaps(){ return gaps; }
     inline unsigned Intervals(){return intervals;}
+
+    std::string toWolframString();
 
 private:
     unsigned gaps, intervals;
